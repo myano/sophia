74ea9f8e33bdbd8eb9ce956ad67ba5203b9a0946
Kays <eureka@divinitycoding.net>
2011-01-01 21:21:23 -0500

    Added in ModuleHandler for handling loading/removing/reloading modules.\nThis is not hooked up to any core feature for loading new modules. That needs to be written.\nPerhaps a src/Sophia.pm?

---
+++ src/ModuleHandler.pm
@@ -0,0 +1,73 @@
+use strict;
+use warnings;
+
+sub sophia_module_load {
+    my $module = $_[0];
+    return -1 unless -e "$Bin/../modules/$module.pm";
+    do "$Bin/../modules/$module.pm" and return 1
+        or return 0;
+}
+
+sub sophia_module_add {
+    my ($module_name, $version, $command_hook, $die) = @_;
+
+    # try loading it
+    eval {
+        # can we hook up the command?
+        my $module = &{$command_hook}();
+        if ($module) {
+            sophia_log('sophia', "[MODULE] $module_name: $module_name v$version successfully loaded.");
+            $sophia::MODULES{$module_name} = (
+                deconstruct     => $die,
+                version         => $version,
+            );
+            return 1;
+            1;
+        }
+        
+        &{$die}();
+        sophia_log('sophia', "[MODULE] $module_name: $module_name v$version failed to load.");
+        return 0;
+    }
+    # something went wrong
+    or sophia_log('sophia', "[MODULE] $module_name: $module_name v$version failed to load.")
+    and return 0;
+}
+
+sub sophia_module_del {
+    my $module_name = $_[0];
+    return -1 unless sophia_module_exists($module_name);
+
+    my $die = $sophia::MODULES{$module_name}{deconstruct};
+    eval {
+        &{$die}();
+        delete_sub $die;
+        sophia_log('sophia', "[MODULE] $module_name: $module_name v".$sophia::MODULES{$module_name}{version}." successfully unloaded.");
+        delete $sophia::MODULES{$module_name};
+        return 1;
+        1;
+    } or sophia_log('sophia', "[MODULE] $module_name: $module_name v".$sophia::MODULES{$module_name}{version}." failed to unload.")
+      and return 0;
+}
+
+sub sophia_module_exists {
+    my $module_name = $_[0];
+    return defined $sophia::MODULES{$module_name};
+}
+
+sub sophia_command_add {
+    my ($module_command, $cmd_hook, $cmd_desc, $cmd_help) = @_;
+    my @mod_cmd = split /\//, $module_command;
+    $sophia::COMMANDS{$mod_cmd[0]}{$mod_cmd[1]} = (
+        init    => $cmd_hook,
+        desc    => $cmd_desc,
+        help    => $cmd_help,
+    );
+}
+
+sub sophia_command_del {
+    my ($module_name, $cmd_name) = @_;;
+    delete $sophia::COMMANDS{$module_name}{$cmd_name};
+}
+
+1;
