63ecee83112bb31876e257af36bbb9b58b2d49c8
Kays <eureka@divinitycoding.net>
2011-04-16 15:55:58 -0400

    Fixed bugs and updated system module.

--- modules/system/main.pm
+++ modules/system/main.pm
@@ -5,10 +5,13 @@ sophia_module_add("system.main", "1.0", \&init_system, \&deinit_system);
 
 sub init_system {
     sophia_command_add("system.mod:reload", \&system_modreload, "Reloads all or a specified module.", "");
-    sophia_command_add("system,mod:load", \&system_modload, "Loads a specified module.", "");
+    sophia_command_add("system.mod:load", \&system_modload, "Loads a specified module.", "");
     sophia_command_add("system.mod:unload", \&system_modunload, "Unloads all or a specified module.", "");
     sophia_command_add("system.restart", \&system_restart, "Restarts sophia.", "");
     sophia_command_add("system.shutdown", \&system_shutdown, "Shutdown sophia.", "");
+
+    sophia_command_add("system.join", \&system_join, "Joins one or more channels.", "");
+    sophia_command_add("system.part", \&system_part, "Parts one or more channels.", "");
 
     return 1;
 }
@@ -35,14 +38,10 @@ sub system_modreload {
     my @parts = split / /, $content;
     shift @parts;
 
-    for (@parts) {
-        if (lc eq "all") {
-            &sophia_reload_modules;
-        }
-        else {
-            sophia_reload_module($_);
-        }
-    }
+    return unless scalar(@parts) > 0;
+
+    sophia_reload_module($_) for @parts;
+    sophia_write(\$where->[0], \sprintf("Module %s reloaded.", join(', ', @parts)));
 }
 
 sub system_modload {
@@ -52,7 +51,10 @@ sub system_modload {
     my @parts = split / /, $content;
     shift @parts;
 
+    return unless scalar(@parts) > 0;
+
     sophia_module_load($_) for @parts;
+    sophia_write(\$where->[0], \sprintf("Module %s loaded.", join(', ', @parts)));
 }
 
 sub system_modunload {
@@ -62,7 +64,10 @@ sub system_modunload {
     my @parts = split / /, $content;
     shift @parts;
 
+    return unless scalar(@parts) > 0;
+
     sophia_module_del($_) for @parts;
+    sophia_write(\$where->[0], \sprintf("Module %s unloaded.", join(', ', @parts)));
 }
 
 sub system_restart {
@@ -73,7 +78,13 @@ sub system_restart {
     sophia_log("sophia", "Restarting sophia by the request of: $who");
     $sophia::sophia->yield(shutdown => "Restarting ...");
     $sophia::sophia->disconnect();
-    `$Bin/sophia`;
+
+    if ($sophia::DEBUG_MODE) {
+        `$Bin/sophia --debug`;
+    }
+    else {
+        `$Bin/sophia`;
+    }
     exit;
 }
 
@@ -86,6 +97,32 @@ sub system_shutdown {
     $sophia::sophia->yield(shutdown => "Shutting down ...");
     $sophia::sophia->disconnect();
     exit;
+}
+
+sub system_join {
+    my $param = $_[0];
+    my @args = @{$param};
+    my ($who, $content) = ($args[ARG0], $args[ARG2]);
+    my @parts = split / /, $content;
+    shift @parts;
+
+    return unless scalar(@parts) > 0;
+
+    sophia_log("sophia", sprintf("Joining (%s) by %s's request.", join(', ', @parts), $who));
+    $sophia::sophia->yield( join => $_ ) for @parts;
+}
+
+sub system_part {
+    my $param = $_[0];
+    my @args = @{$param};
+    my ($who, $where, $content) = @args[ARG0 .. ARG2];
+    my @parts = split / /, $content;
+    shift @parts;
+
+    $sophia::sophia->yield( part => $where->[0] ) and return unless scalar(@parts);
+
+    sophia_log("sophia", sprintf("Parting (%s) by %s's request.", join(', ', @parts), $who));
+    $sophia::sophia->yield( part => $_ ) for @parts;
 }
 
 1;
