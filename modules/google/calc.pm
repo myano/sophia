42e9994c75fe7e39970f97962834a24158c38246
Kays <eureka@divinitycoding.net>
2011-04-18 01:41:09 -0400

    Add google module. This closes issue: #5, #6

---
+++ modules/google/calc.pm
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+
+sophia_module_add('google.calc', '1.0', \&init_google_calc, \&deinit_google_calc);
+
+sub init_google_calc {
+    sophia_command_add('google.calc', \&google_calc, 'Uses Google for calculating stuff.', '');
+
+    return 1;
+}
+
+sub deinit_google_calc {
+    delete_sub 'init_google_calc';
+    delete_sub 'google_calc';
+    sophia_command_del 'google.calc';
+    delete_sub 'deinit_google_calc';
+}
+
+sub google_calc {
+    my $param = $_[0];
+    my @args = @{$param};
+    my ($where, $content) = ($args[ARG1], $args[ARG2]);
+    $content = substr $content, index($content, ' ') + 1;
+
+    $content =~ s/\+/\%2B/g;
+    $content =~ s/\^/\%5E/g;
+    $content =~ s/ /\+/g;
+
+    my $objHTML = get_file_contents(\sprintf('http://www.google.com/search?q=%s', $content));
+    $objHTML = ${$objHTML};
+
+    if ($objHTML =~ /<h(2|3) class=r( [^>]+)?><b>(.+?) = (.+?)<\/b><\/h(2|3)>/) {
+        my ($eq, $ans) = ($3, $4);
+        $eq =~ s/<font size=-2> <\/font>/,/g;
+        $ans =~ s/<sup>(\d+)<\/sup>/^$1/g;
+        $ans =~ s/<font size=-2> <\/font>/,/g;
+
+        sophia_write( \$where->[0], \sprintf('%s = %s', $eq, $ans) );
+    }
+}
+
+1;
---
+++ modules/google/translate.pm
@@ -0,0 +1,45 @@
+use strict;
+use warnings;
+
+sophia_module_add('google.translate', '1.0', \&init_google_translate, \&deinit_google_translate);
+
+sub init_google_translate {
+    sophia_command_add('google.translate', \&google_translate, 'Utilize Google for translations.', '');
+
+    return 1;
+}
+
+sub deinit_google_translate {
+    delete_sub 'init_google_translate';
+    delete_sub 'google_translate';
+    sophia_command_del 'google.translate';
+    delete_sub 'deinit_google_translate';
+}
+
+sub google_translate {
+    my $param = $_[0];
+    my @args = @{$param};
+    my ($where, $content) = @args[ARG1 .. ARG2];
+    $content = substr $content, index($content, ' ') + 1;
+    $content =~ s/^\s+//;
+
+    my $lang = substr $content, 0, index($content, ' ');
+    my $text = substr $content, index($content, ' ') + 1;
+
+    $lang =~ s/ /+/g;
+    $text =~ s/ /+/g;
+
+    my $objHTTP = get_file_contents(\sprintf('http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=%s&langpair=%s', $text, $lang));
+    $objHTTP = ${$objHTTP};
+
+    if ($objHTTP =~ m/{"translatedText":"([^"]+)"}/) {
+        my $val = $1;
+        $val =~ s/\\u0026/&/g;
+        sophia_write( \$where->[0], \$val );
+    }
+
+    ($objHTTP, $content) = undef;
+    @args = undef;
+}
+
+1;
