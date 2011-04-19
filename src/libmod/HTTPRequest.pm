42f18f7aa13903cc2e1424d24933829b281d3bbc
Kays <eureka@divinitycoding.net>
2011-04-19 01:11:48 -0400

    Added module ln-s. Closes #8.

--- src/libmod/HTTPRequest.pm
+++ src/libmod/HTTPRequest.pm
@@ -23,7 +23,7 @@ sub get_file_contents {
     return unless ($url =~ m/^http:\/\/[^ ]+$/);
 
     $url = URI::Heuristic::uf_urlstr($url);
-    my $ua = LWP::UserAgent->new();
+    my $ua = LWP::UserAgent->new;
     $ua->agent("Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 GTB5");
 
     my $req = HTTP::Request->new( GET => $url );
@@ -31,6 +31,27 @@ sub get_file_contents {
 
     return if ($response->is_error());
     return \$response->content;
+}
+
+sub get_lns {
+    my $uri = $_[0];
+    $uri = ${$uri};
+    $uri = URI::Heuristic::uf_urlstr($uri);
+
+    my $ua = LWP::UserAgent->new;
+    my $req = HTTP::Request->new( POST => 'http://ln-s.net/home/api.jsp' );
+    $req->content_type('application/x-www-form-urlencoded');
+    $req->content("url=$uri");
+
+    my $response = $ua->request($req);
+    return if $response->is_error();
+
+    my $content = $response->content;
+
+    my @parts = split / /, $content;
+    return unless $parts[0] eq '200';
+
+    return \$parts[1];
 }
 
 1;
