45fa14d31a6f6e9da5a065ab9a3f06e9952e2928
Michael S. Yanovich <yanovich.1@osu.edu>
2011-04-20 18:18:27 -0400

    added time module

---
+++ modules/common/time.pm
@@ -0,0 +1,276 @@
+use strict;
+use warnings;
+
+sophia_module_add('common.time', '1.0', \&init_common_time, \&deinit_common_time);
+
+sub init_common_time {
+    sophia_command_add('common.time', \&common_time, 'Print the current time.', 'Prints the current time. If no paramters are given it prints GMT.');
+    sophia_global_command_add('time', \&common_time, 'Print the current time', 'Prints the current time. If no parameters are given it prints GMT.');
+
+    return 1;
+}
+
+sub deinit_common_time {
+    delete_sub 'init_common_time';
+    delete_sub 'common_time';
+    sophia_command_del 'common.time';
+    delete_sub 'deinit_common_time';
+}
+
+sub common_time {
+    my $param = $_[0];
+    my @args = @{$param};
+    my ($where, $content) = ($args[ARG1], $args[ARG2]);
+
+    my %timezones = 
+    (
+        'KST' => 9,
+        'CADT' => 10.5,
+        'EETDST' => 3,
+        'MESZ' => 2,
+        'WADT' => 9,
+        'EET' => 2,
+        'MST' => -7,
+        'WAST' => 8,
+        'IST' => 5.5,
+        'B' => 2,
+        'MSK' => 3,
+        'X' => -11,
+        'MSD' => 4,
+        'CETDST' => 2,
+        'AST' => -4,
+        'HKT' => 8,
+        'JST' => 9,
+        'CAST' => 9.5,
+        'CET' => 1,
+        'CEST' => 2,
+        'EEST' => 3,
+        'EAST' => 10,
+        'METDST' => 2,
+        'MDT' => -6,
+        'A' => 1,
+        'UTC' => 0,
+        'ADT' => -3,
+        'EST' => -5,
+        'E' => 5,
+        'D' => 4,
+        'G' => 7,
+        'F' => 6,
+        'I' => 9,
+        'H' => 8,
+        'K' => 10,
+        'PDT' => -7,
+        'M' => 12,
+        'L' => 11,
+        'O' => -2,
+        'MEST' => 2,
+        'Q' => -4,
+        'P' => -3,
+        'S' => -6,
+        'R' => -5,
+        'U' => -8,
+        'T' => -7,
+        'W' => -10,
+        'WET' => 0,
+        'Y' => -12,
+        'CST' => -6,
+        'EADT' => 11,
+        'Z' => 0,
+        'GMT' => 0,
+        'WETDST' => 1,
+        'C' => 3,
+        'WEST' => 1,
+        'CDT' => -5,
+        'MET' => 1,
+        'N' => -1,
+        'V' => -9,
+        'EDT' => -4,
+        'UT' => 0,
+        'PST' => -8,
+        'MEZ' => 1,
+        'BST' => 1,
+        'ACS' => 9.5,
+        'ATL' => -4,
+        'ALA' => -9,
+        'HAW' => -10,
+        'AKDT' => -8,
+        'AKST' => -9, 
+        'BDST' => 2,
+        'NDT' => -2.5, 
+        'BRST' => -2, 
+        'ADT' => -3, 
+        'EDT' => -4, 
+        'CDT' => -5, 
+        'MDT' => -6, 
+        'PDT' => -7, 
+        'YDT' => -8, 
+        'HDT' => -9, 
+        'BST' => 1, 
+        'MEST' => 2, 
+        'SST' => 2, 
+        'FST' => 2, 
+        'CEST' => 2, 
+        'EEST' => 3, 
+        'WADT' => 8, 
+        'KDT' => 10, 
+        'EADT' => 13, 
+        'NZD' => 13, 
+        'NZDT' => 13, 
+        'GMT' => 0, 
+        'UT' => 0, 
+        'UTC' => 0, 
+        'WET' => 0, 
+        'WAT' => -1, 
+        'AT' => -2, 
+        'FNT' => -2, 
+        'BRT' => -3, 
+        'MNT' => -4, 
+        'EWT' => -4, 
+        'AST' => -4, 
+        'EST' => -5, 
+        'ACT' => -5, 
+        'CST' => -6, 
+        'MST' => -7, 
+        'PST' => -8, 
+        'YST' => -9, 
+        'HST' => -10, 
+        'CAT' => -10, 
+        'AHST' => -10, 
+        'NT' => -11, 
+        'IDLW' => -12, 
+        'CET' => 1, 
+        'MEZ' => 1, 
+        'ECT' => 1, 
+        'MET' => 1, 
+        'MEWT' => 1, 
+        'SWT' => 1, 
+        'SET' => 1, 
+        'FWT' => 1, 
+        'EET' => 2, 
+        'UKR' => 2, 
+        'BT' => 3, 
+        'ZP4' => 4, 
+        'ZP5' => 5, 
+        'ZP6' => 6, 
+        'WST' => 8, 
+        'HKT' => 8, 
+        'CCT' => 8, 
+        'JST' => 9, 
+        'KST' => 9, 
+        'EAST' => 10, 
+        'GST' => 10, 
+        'NZT' => 12, 
+        'NZST' => 12, 
+        'IDLE' => 12,
+        'ACDT' => 10.5, 
+        'ACST' => 9.5, 
+        'ADT' => 3, 
+        'AEDT' => 11, # hmm
+        'AEST' => 10, # hmm
+        'AHDT' => 9, 
+        'AHST' => 10, 
+        'AST' => 4, 
+        'AT' => 2, 
+        'AWDT' => -9, 
+        'AWST' => -8, 
+        'BAT' => -3, 
+        'BDST' => -2, 
+        'BET' => 11, 
+        'BST' => -1, 
+        'BT' => -3, 
+        'BZT2' => 3, 
+        'CADT' => -10.5, 
+        'CAST' => -9.5, 
+        'CAT' => 10, 
+        'CCT' => -8, 
+        # 'CDT' => 5, 
+        'CED' => -2, 
+        'CET' => -1, 
+        'CST' => 6, 
+        'EAST' => -10, 
+        # 'EDT' => 4, 
+        'EED' => -3, 
+        'EET' => -2, 
+        'EEST' => -3, 
+        'EST' => 5, 
+        'FST' => -2, 
+        'FWT' => -1, 
+        'GMT' => 0, 
+        'GST' => -10, 
+        'HDT' => 9, 
+        'HST' => 10, 
+        'IDLE' => -12, 
+        'IDLW' => 12, 
+        # 'IST' => -5.5, 
+        'IT' => -3.5, 
+        'JST' => -9, 
+        'JT' => -7, 
+        'KST' => -9, 
+        'MDT' => 6, 
+        'MED' => -2, 
+        'MET' => -1, 
+        'MEST' => -2, 
+        'MEWT' => -1, 
+        'MST' => 7, 
+        'MT' => -8, 
+        'NDT' => 2.5, 
+        'NFT' => 3.5, 
+        'NT' => 11, 
+        'NST' => -6.5, 
+        'NZ' => -11, 
+        'NZST' => -12, 
+        'NZDT' => -13, 
+        'NZT' => -12, 
+        # 'PDT' => 7, 
+        'PST' => 8, 
+        'ROK' => -9, 
+        'SAD' => -10, 
+        'SAST' => -9, 
+        'SAT' => -9, 
+        'SDT' => -10, 
+        'SST' => -2, 
+        'SWT' => -1, 
+        'USZ3' => -4, 
+        'USZ4' => -5, 
+        'USZ5' => -6, 
+        'USZ6' => -7, 
+        'UT' => 0, 
+        'UTC' => 0, 
+        'UZ10' => -11, 
+        'WAT' => 1, 
+        'WET' => 0, 
+        'WST' => -8, 
+        'YDT' => 8, 
+        'YST' => 9, 
+        'ZP4' => -4, 
+        'ZP5' => -5, 
+        'ZP6' => -6,
+        'AEST' => 10, 
+        'AEDT' => 11
+    );
+
+    my $idx = index $content, ' ';
+    $content = $idx > -1 ? substr($content, $idx + 1) : '';
+
+    if ( $content =~ /^[+-]?\d+$/ ) {
+        my $offset = $content;
+        if ($offset > 100000) {
+            return;
+        }
+        my $offsetsec = $offset * 3600;
+        sophia_write( \$where->[0], \sprintf('%s %s', scalar(gmtime(time() + $offsetsec)), $offset) );
+    }
+    else {
+        $content = uc $content;
+        if (defined $timezones{$content}) {
+            my $offset = $timezones{$content};
+            $offset = $offset * 3600;
+            sophia_write( \$where->[0], \sprintf('%s %s', scalar(gmtime(time() + $offset)), $content) );
+        }
+        else {
+            sophia_write( \$where->[0], \sprintf('%s GMT', scalar(gmtime(time()))) );
+        }
+    }
+}
+
+1;
