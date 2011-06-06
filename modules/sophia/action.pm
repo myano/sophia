c98e5130a555e13f12c6b151a64c011e306e5a72
Kenneth K. Sham <eureka@divinitycoding.net>
2011-06-05 21:03:05 -0400

    Fixed action and msg commands. $target should only be used to determine if the event is a privmsg, not the recipient of the output.

--- modules/sophia/action.pm
+++ modules/sophia/action.pm
@@ -4,7 +4,7 @@ use warnings;
 sophia_module_add('sophia.action', '1.0', \&init_sophia_action, \&deinit_sophia_action);
 
 sub init_sophia_action {
-    sophia_global_command_add('action', \&sophia_action, 'Sends an action.', '');
+    sophia_global_command_add('act', \&sophia_action, 'Sends an action.', '');
     sophia_event_privmsg_hook('sophia.action', \&sophia_action, 'Sends an action.', '', SOPHIA_ACL_ADMIN);
 
     1;
@@ -21,27 +21,24 @@ sub deinit_sophia_action {
 sub sophia_action {
     my ($args, $target) = @_;
     my ($where, $content) = ($args->[ARG1], $args->[ARG2]);
-    $target //= $where->[0];
-
-    my $self = &sophia_get_config;
-    my $isSelf = lc $target eq lc $self->{nick};
+    my $recipient = $where->[0];
 
     my $idx = index $content, ' ';
     return unless $idx > -1;
 
     $content = substr $content, $idx + 1;
 
-    # if the target is sophia, get first arg as the target
-    if ($isSelf) {
+    # if this is a privmsg
+    if ($target) {
         $content =~ s/^\s+//;
         $idx = index $content, ' ';
         return unless $idx > -1;
-        $target = substr $content, 0, $idx;
+        $recipient = substr $content, 0, $idx;
         $content = substr $content, $idx + 1;
     }
 
     my $sophia = ${$args->[HEAP]->{sophia}};
-    $sophia->yield(ctcp => $target => sprintf('ACTION %s', $content));
+    $sophia->yield(ctcp => $recipient => sprintf('ACTION %s', $content));
 }
 
 1;
