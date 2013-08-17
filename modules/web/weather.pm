bec22d55780cdd4ca752ccb99f51962c49682a67
Kenneth K. Sham <ken@ksham.net>
2013-08-16 21:03:22 -0400

    convert web::weather to new framework

--- modules/web/weather.pm
+++ modules/web/weather.pm
@@ -1,51 +1,58 @@
-use strict;
-use warnings;
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
 
-sophia_module_add('web.weather', '1.0', \&init_web_weather, \&deinit_web_weather);
+class web::weather with API::Module
+{
+    use URI::Escape;
+    use Util::String;
+    use Util::XML;
 
-sub init_web_weather {
-    sophia_command_add('web.weather', \&web_weather, 'Gets weather from api.wunderground.com', '');
+    has 'name'  => (
+        default => 'web::weather',
+        is      => 'ro',
+        isa     => 'Str',
+    );
 
-    return 1;
+    has 'version'  => (
+        default     => '1.0',
+        is          => 'ro',
+        isa         => 'Str',
+    );
+
+    method run ($event)
+    {
+        my $content = Util::String->trim($event->content);
+        my @dataset;
+
+        my $results = $self->weather($content);
+        for my $result (@$results)
+        {
+            push @dataset, $result->[0]->to_literal;
+        }
+
+        $event->reply( sprintf('%s (%s)  --  %s, %s.  Humidity: %s.  Wind: %s, %s degrees at %s mph.  Dewpoint: %s.  Air pressure: %s.  Visibility: %s miles.', @dataset) );
+    }
+
+    method weather ($location)
+    {
+        my $xml = Util::XML->new;
+        $xml->parse_by_url('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=' . uri_escape($location));
+        return unless $xml;
+
+        my @nodes = (
+            '//display_location/full',
+            '//station_id',
+            '//weather',
+            '//temperature_string',
+            '//relative_humidity',
+            '//wind_dir',
+            '//wind_degrees',
+            '//wind_mph',
+            '//dewpoint_string',
+            '//pressure_string',
+            '//visibility_mi',
+        );
+
+        return $xml->get_nodes(\@nodes);
+    }
 }
-
-sub deinit_web_weather {
-    delete_sub 'init_web_weather';
-    delete_sub 'web_weather';
-    delete_sub 'web_weather_getData';
-    sophia_command_del 'web.weather';
-    delete_sub 'deinit_web_weather';
-}
-
-sub web_weather_getData {
-    my ($obj, @selectors) = @_;
-    $obj = ${$obj};
-
-    my @results;
-    push @results, $obj->findnodes($_)->[0]->to_literal for @selectors;
-    return \@results;
-}
-
-sub web_weather {
-    my $args = $_[0];
-    my ($where, $content) = ($args->[ARG1], $args->[ARG2]);
-    my $idx = index $content, ' ';
-    return if $idx == -1;
-
-    $content = substr $content, $idx + 1;
-    $content =~ s/ /+/g;
-
-    my $objXML = loadXML(sprintf('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=%s', $content));
-    return if !$objXML;
-    $objXML = ${$objXML};
-
-    my $dataset = web_weather_getData(\$objXML, '//display_location/full', '//station_id', '//weather', '//temperature_string', '//relative_humidity', '//wind_dir', '//wind_degrees', '//wind_mph', '//dewpoint_string', '//pressure_string', '//visibility_mi');
-    return if $dataset->[0] eq ', ';
-
-    my $result = sprintf('%s (%s)  --  %s, %s.  Humidity: %s.  Wind: %s, %s degrees at %s mph.  Dewpoint: %s.  Air pressure: %s.  Visibility: %s miles.', @{$dataset});
-    
-    my $sophia = $args->[HEAP]->{sophia};
-    $sophia->yield(privmsg => $where->[0] => $result);
-}
-
-1;
