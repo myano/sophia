d785b1e4f40bf8581f83a0abe034bedd97f242ff
Kenneth K. Sham <ken@ksham.net>
2014-05-20 01:37:05 -0400

    Update curl lib. Use IO::Uncompress::Gunzip to gunzip gzipped data. Also add a download function to allow downloading of page content to disk.

--- lib/Util/Curl.pm
+++ lib/Util/Curl.pm
@@ -3,8 +3,11 @@ use Method::Signatures::Modifiers;
 
 class Util::Curl
 {
+    use constant USERAGENT => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36';
+
     use API::Log qw(:ALL);
     use Constants;
+    use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
     use WWW::Curl::Easy;
 
     has 'postheaders'   => (
@@ -15,18 +18,29 @@ class Util::Curl
 
     method get ($uri)
     {
-        return unless $uri =~ /\Ahttps?:\/\/[^ ]+\z/;
+        return unless $uri =~ /\Ahttps?:\/\/(www\.)?([^ \/]+)[^ ]*\z/;
+
+        my @headers = (
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding: gzip,deflate,sdch',
+            'Accept-Language: en-US,en;q=0.8,zh;q=0.6,zh-CN;q=0.4,zh-TW;q=0.2,fr;q=0.2,fr-FR;q=0.2,ja;q=0.2,es;q=0.2',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Host: ' . $2,
+            'User-Agent: ' . USERAGENT,
+        );
 
         my $curl = WWW::Curl::Easy->new;
+        $curl->setopt(CURLOPT_AUTOREFERER, 1);
         $curl->setopt(CURLOPT_CONNECTTIMEOUT, 7);
-        $curl->setopt(CURLOPT_COOKIEFILE, 'cookie.txt');
-        $curl->setopt(CURLOPT_COOKIEJAR,  'cookie.txt');
+        $curl->setopt(CURLOPT_COOKIEFILE, $sophia::BASE{ETC} . '/cookies.txt');
+        $curl->setopt(CURLOPT_COOKIEJAR, $sophia::BASE{ETC} . '/cookies.txt');
         $curl->setopt(CURLOPT_COOKIESESSION, 1);
         $curl->setopt(CURLOPT_FOLLOWLOCATION, 1);
         $curl->setopt(CURLOPT_HEADER, 0);
+        $curl->setopt(CURLOPT_HTTPHEADER, \@headers);
         $curl->setopt(CURLOPT_TIMEOUT, 5);
         $curl->setopt(CURLOPT_URL, $uri);
-        $curl->setopt(CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0');
         $curl->setopt(CURLOPT_VERBOSE, TRUE);
 
         my $response;
@@ -35,10 +49,61 @@ class Util::Curl
         my $retcode = $curl->perform;
         if ($retcode == 0)
         {
-            return $response;
+            my $gresponse;
+            gunzip \$response => \$gresponse;
+            return $gresponse;
         }
 
         _log('sophia', sprintf('[Util::Curl::get] An error occured. retcode: %s. Error: %s %s', $retcode, $curl->strerror($retcode), $curl->errbuf));
+
+        return;
+    }
+
+    method download ($uri, $path, $gpath = '')
+    {
+        return unless $uri =~ /\Ahttps?:\/\/(www\.)?([^ \/]+)[^ ]*\z/;
+
+        my @headers = (
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding: gzip,deflate,sdch',
+            'Accept-Language: en-US,en;q=0.8,zh;q=0.6,zh-CN;q=0.4,zh-TW;q=0.2,fr;q=0.2,fr-FR;q=0.2,ja;q=0.2,es;q=0.2',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Host: ' . $2,
+            'User-Agent: ' . USERAGENT,
+        );
+
+        open(my $fh, '>', $path) or _log('sophia', sprintf('[Util::Curl::download] Unable to open download path: %s', $!)) and return;
+
+        my $curl = WWW::Curl::Easy->new;
+        $curl->setopt(CURLOPT_AUTOREFERER, 1);
+        $curl->setopt(CURLOPT_CONNECTTIMEOUT, 7);
+        $curl->setopt(CURLOPT_COOKIEFILE, $sophia::BASE{ETC} . '/cookies.txt');
+        $curl->setopt(CURLOPT_COOKIEJAR, $sophia::BASE{ETC} . '/cookies.txt');
+        $curl->setopt(CURLOPT_COOKIESESSION, 1);
+        $curl->setopt(CURLOPT_FOLLOWLOCATION, 1);
+        $curl->setopt(CURLOPT_HEADER, 0);
+        $curl->setopt(CURLOPT_HTTPHEADER, \@headers);
+        $curl->setopt(CURLOPT_TIMEOUT, 5);
+        $curl->setopt(CURLOPT_URL, $uri);
+        $curl->setopt(CURLOPT_VERBOSE, TRUE);
+        $curl->setopt(CURLOPT_WRITEDATA, $fh);
+        
+        my $retcode = $curl->perform;
+        close $fh;
+
+        if ($retcode == 0)
+        {
+            if ($gpath)
+            {
+                gunzip $path => $gpath or _log('sophia', sprintf('[Util::Curl::download] Unable to decompress gzip file: %s', $GunzipError));
+            }
+
+            return 1;
+        }
+
+        _log('sophia', sprintf('[Util::Curl::download] An error occured. retcode: %s. Error: %s %s', $retcode, $curl->strerror($retcode), $curl->errbuf));
+
         return;
     }
 
