11d07808488af4087ce266511f129d58a4b67750
Kenneth K. Sham <ken@ksham.net>
2014-05-22 22:25:27 -0400

    Add method to determine if sender is an operator.

--- lib/Protocol/IRC/Event.pm
+++ lib/Protocol/IRC/Event.pm
@@ -20,4 +20,31 @@ role Protocol::IRC::Event
     {
         return $self->DOES('Protocol::IRC::Event::Public');
     }
+
+    method is_sender_operator ()
+    {
+        my %operators = %{$sophia::instances->operators};
+        my $sender    = $self->sender;
+        my $hostmask  = $sender;
+
+        my $idx = index $sender, '!';
+        if ($idx != -1)
+        {
+            $hostmask = substr $sender, $idx + 1;
+        }
+
+        while (my ($user, $data) = each %operators)
+        {
+            # has hostmask?
+            if (exists $data->{hostmask})
+            {
+                if ($data->{hostmask} eq $hostmask)
+                {
+                    return TRUE;
+                }
+            }
+        }
+
+        return FALSE;
+    }
 }
